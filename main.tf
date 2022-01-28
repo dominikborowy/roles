b9624742fba8f23cf6f5ca1d8f47fd87068aa614
dominikborowy <96127494+dominikborowy@users.noreply.github.com>
2022-01-28 11:47:32 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_iam_role" "roles" {
+  for_each = {
+    for role, value in var.role_dict : role => value if !contains(var.roles_to_omit, role)
+  }
+  name               = "${var.user-prefix}-${each.key}"
+  assume_role_policy = file(each.value[1][0])
+  managed_policy_arns = [for policy in each.value[0]: "arn:aws:iam::aws:policy/${policy}"]
+}
